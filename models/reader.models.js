4ab4a8c44bf9f4cd26d5a45113b081a1512ba5a7
HoangHan594 <nguyenhoanghan2003@gmail.com>
2024-09-03 23:51:29 +0700

    models

---
+++ models/reader.models.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const slug = require('mongoose-slug-updater');
+mongoose.plugin(slug);
+
+const readerSchema = new mongoose.Schema({
+    fullName: String,
+    email: String,
+    password: String,
+    token: String,
+    address: String,
+    phone: String,
+    borrow: [{
+        id_book: String,
+        status: {
+            type: String,
+            default: "processing" //processing accepted refused returned
+        },
+        borrowDate: String,
+        returnDate: String,
+        quantity: {
+            type: Number,
+            default: 1,
+            require: true,
+        },
+    }],
+    accountBalance: Number,
+    bankAccount: {
+        accountNumber: String,
+        bankName: String,
+        accountHolderName: String
+    },
+    resetPasswordToken: String,
+    resetPasswordExpires: String,
+    deleted: {
+        type: Boolean,
+        default: false
+    },
+    deletedAt: Date,
+}, { timestamps: true })
+
+const Reader = mongoose.model("Reader", readerSchema, "readers");
+
+module.exports = Reader;
